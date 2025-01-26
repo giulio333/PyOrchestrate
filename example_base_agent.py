2e079dc9dc374a95c25a535c22ea720e7ad5914d
giulio333 <digiagiulio@gmail.com>
2025-01-26 13:29:10 +0100

    Add ZeroMQ communication support for LogMonitorAgent and LogReceiverAgent example

--- example_base_agent.py
+++ example_base_agent.py
@@ -1,6 +1,10 @@
 from PyOrchestrate.core.orchestrator import Orchestrator
 from PyOrchestrate.core.orchestrator import AgentEntry
 from PyOrchestrate.core.agent import BaseProcessAgent
+from PyOrchestrate.core.plugins.communication_plugins import ZeroMQPlugin
+import zmq
+import time
+import multiprocessing
 
 
 class MyConfig(BaseProcessAgent.Config):
@@ -18,17 +22,21 @@ class LogMonitorAgent(BaseProcessAgent[MyConfig]):
         """
         super().setup()
 
-        self.config.logger_config.level = "INFO"
+        zmqPlugin = ZeroMQPlugin("tcp://localhost:5555", zmq.PUB)
+        self.register_plugin(zmqPlugin)
 
         self.logger.info(
-            f"Initializing LogMonitorAgent for file: {self.config.log_file}"
+            f"Initializing LogMonitorAgent for file: {self.config.log_file}."
         )
+
+        self.logger.info("Waiting for receiver to connect...")
+        time.sleep(1)
+
         try:
             with open(self.config.log_file, "r") as f:
                 self.logger.info("Log file found.")
         except FileNotFoundError:
             self.logger.error(f"Log file {self.config.log_file} does not exist.")
-            raise
 
     def execute(self) -> None:
         """
@@ -42,8 +50,15 @@ class LogMonitorAgent(BaseProcessAgent[MyConfig]):
                 for line in f:
                     if self.config.keyword in line:
                         self.logger.warning(f"Keyword found: {line.strip()}")
+                        cm = self.get_plugin("ZeroMQPlugin")
+                        cm.send(f"Keyword found: {line.strip()}")  # type: ignore
+
         except Exception as e:
-            self.logger.error(f"Error reading the log file: {e}")
+            self.logger.exception(f"Error reading the log file: {e}")
+        finally:
+            cm = self.get_plugin("ZeroMQPlugin")
+            cm.send("STOP")  # type: ignore
+            cm.finalize()
 
     def on_stop(self):
         """
@@ -52,10 +67,60 @@ class LogMonitorAgent(BaseProcessAgent[MyConfig]):
         self.logger.info("LogMonitorAgent stopped.")
 
 
+class LogReceiverAgent(BaseProcessAgent[MyConfig]):
+    Config = MyConfig
+
+    def setup(self) -> None:
+        """
+        Ensure the log file exists.
+        """
+        super().setup()
+
+        zmqPlugin = ZeroMQPlugin("tcp://localhost:5555", zmq.SUB)
+        self.register_plugin(zmqPlugin)
+
+        self.config.logger_config.level = "INFO"
+
+        self.logger.info(
+            f"Initializing LogReceiverAgent for file: {self.config.log_file}"
+        )
+
+    def execute(self) -> None:
+        """
+        Monitor the log file for the specified keyword.
+        """
+        super().execute()
+
+        self.logger.info(f"Monitoring for keyword: '{self.config.keyword}'")
+        try:
+            cm = self.get_plugin("ZeroMQPlugin")
+            while True:
+                message = cm.receive()  # type: ignore
+                self.logger.success(f"Received message: {message}")
+
+                if message == "STOP":
+                    break
+
+        except Exception as e:
+            self.logger.exception(f"Error reading the log file: {e}")
+        finally:
+            cm = self.get_plugin("ZeroMQPlugin")
+            cm.finalize()
+
+    def on_stop(self):
+        """
+        Log the agent's shutdown.
+        """
+        self.logger.info("LogReceiverAgent stopped.")
+
+
 if __name__ == "__main__":
     orchestrator = Orchestrator()
 
     # register agents
+    lr_agent: AgentEntry = orchestrator.register_agent(
+        LogReceiverAgent, "LogReceiverAgent"
+    )
     fw_agent: AgentEntry = orchestrator.register_agent(
         LogMonitorAgent, "LogMonitorAgent"
     )
