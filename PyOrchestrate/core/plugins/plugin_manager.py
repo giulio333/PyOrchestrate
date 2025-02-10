bb24a4aa85b474f72cee2844dfec30cddae7f0f0
giulio333 <digiagiulio@gmail.com>
2025-02-10 18:56:48 +0100

    Enhance BaseAgent and PluginManager with improved documentation and plugin support
    - Added `plugin` attribute to `BaseAgent` for better extensibility.
    - Updated constructor of `BaseAgent` to include `plugin` parameter.
    - Enhanced `PluginManager` with detailed docstrings for clarity.
    - Implemented methods for initializing and finalizing plugins in `PluginManager`.
    - Improved overall code readability and maintainability through documentation.

--- PyOrchestrate/core/plugins/plugin_manager.py
+++ PyOrchestrate/core/plugins/plugin_manager.py
@@ -1,16 +1,36 @@
+"""Module for managing plugin initialization and finalization."""
+
 from PyOrchestrate.core.plugins.plugin_protocols import PluginProtocol
 
 
 class PluginManager:
+    """Class responsible for managing plugins, providing methods for initialization and finalization."""
+
     def __init__(self, plugins):
+        """
+        Initialize the PluginManager with the provided plugins instance.
+
+        Parameters:
+        plugins: An object containing plugin instances defined as attributes.
+        """
         self.plugins = plugins
 
     def initialize_plugins(self):
+        """
+        Initialize all plugins that implement the PluginProtocol.
+
+        Iterates over the plugin attributes and calls the initialize method on each plugin.
+        """
         for key, value in self.plugins.__class__.__dict__.items():
             if isinstance(value, PluginProtocol):
                 value.initialize()
 
     def finalize_plugins(self):
+        """
+        Finalize all plugins that implement the PluginProtocol.
+
+        Iterates over the plugin attributes and calls the finalize method on each plugin.
+        """
         for key, value in self.plugins.__class__.__dict__.items():
             if isinstance(value, PluginProtocol):
                 value.finalize()
