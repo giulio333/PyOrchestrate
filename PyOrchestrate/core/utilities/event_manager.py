5e40263a80736f1100069ff33bc837071d9f5f9d
giulio333 <digiagiulio@gmail.com>
2025-05-04 11:26:29 +0200

    Remove unused import in event_manager.py

--- PyOrchestrate/core/utilities/event_manager.py
+++ PyOrchestrate/core/utilities/event_manager.py
@@ -12,7 +12,6 @@ It isolates errors in one callback so that they do not affect the execution of o
 from enum import Enum
 import inspect
 import logging
-import threading
 import atexit
 from datetime import datetime
 from typing import Callable, Dict, List, Optional
